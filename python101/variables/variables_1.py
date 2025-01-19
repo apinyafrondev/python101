394d75260c5d038621463b09efd21b3ffc231ba0
apinyafrondev <apinya_codecreator@outlook.com>
2025-01-19 17:11:24 +0700

    first commit content for create variable in python:)

---
+++ python101/variables/variables_1.py
@@ -0,0 +1,17 @@
+uuid = '3y4938eryqwueykwashe23y9849quey'
+firstname = 'Apinya'
+lastname = 'Forndev'
+email = 'example@examplemail.localhost'
+age = 25
+weight = 65.5
+height = 175
+gender = 'male'
+
+print(uuid)
+print(firstname)
+print(lastname)
+print(email)
+print(age)
+print(weight)
+print(height)
+print(gender)
